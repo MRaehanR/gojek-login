82ccfd630d3660c34d06e01678ff14324f418092
MRaehanR <raehanrobban2@gmail.com>
2022-08-23 20:58:16 +0700

    fixing

--- lib/login.dart
+++ lib/login.dart
@@ -12,16 +12,18 @@ class login extends StatefulWidget {
 class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
         body: Center(
       child: Column(children: [
         Container(
-          margin: const EdgeInsets.only(top: 230),
+          margin: EdgeInsets.only(top: width * 0.665),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(50),
             child: Image.asset(
               'assets/gojek-illustration.jpeg',
-              width: 350,
+              width: width * 0.9,
             ),
           ),
         ),
@@ -30,7 +32,7 @@ class _loginState extends State<login> {
           margin: const EdgeInsets.only(top: 50, left: 20),
           child: Image.asset(
             'assets/tulisan-gojek.jpeg',
-            width: 100,
+            width: width * 0.2,
           ),
         ),
         Container(
@@ -38,41 +40,49 @@ class _loginState extends State<login> {
           margin: const EdgeInsets.only(left: 25, top: 15),
           child: const Text(
             'Welcome, Keith!',
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
           ),
         ),
         Container(
           alignment: Alignment.topLeft,
-          margin: const EdgeInsets.only(left: 25, top: 15),
+          margin: const EdgeInsets.only(left: 25, top: 15, right: 50),
           child: const Text(
-              'Discover a hassle-free life with the super app for all your needs.'),
+            'Discover a hassle-free life with the super app for all your needs.',
+            style: TextStyle(fontSize: 18.0),
+          ),
         ),
         Container(
-          margin: const EdgeInsets.only(top: 25),
+          margin: const EdgeInsets.only(top: 30),
           child: ElevatedButton(
             onPressed: () {},
             style: ElevatedButton.styleFrom(
                 primary: const Color.fromRGBO(22, 121, 29, 1),
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 120, vertical: 15),
+                    EdgeInsets.symmetric(horizontal: width * 0.31, vertical: 16),
                 textStyle: const TextStyle(fontWeight: FontWeight.bold),
                 shape: const StadiumBorder()),
-            child: const Text('Login as Keith'),
+            child: const Text(
+              'Login as Keith',
+              style: TextStyle(fontSize: 16.0),
+            ),
           ),
         ),
         Container(
-          margin: const EdgeInsets.only(top: 10),
+          margin: const EdgeInsets.only(top: 15),
           child: OutlinedButton(
             onPressed: () {},
             style: OutlinedButton.styleFrom(
                 primary: const Color.fromRGBO(22, 121, 29, 1),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                padding: EdgeInsets.symmetric(
+                    horizontal: width * 0.2, vertical: 16),
                 textStyle: const TextStyle(fontWeight: FontWeight.bold),
                 shape: const StadiumBorder(),
                 side: const BorderSide(
                     width: 1.5, color: Color.fromRGBO(22, 121, 29, 1))),
-            child: const Text('Login with phone number'),
+            child: const Text(
+              'Login with phone number',
+              style: TextStyle(fontSize: 16.0),
+            ),
           ),
         ),
         Container(
@@ -85,19 +95,21 @@ class _loginState extends State<login> {
           child: RichText(
               text: const TextSpan(
                   text: 'By logging in or registering , you agree to our',
-                  style: TextStyle(color: Colors.black),
+                  style: TextStyle(color: Colors.black, fontSize: 13),
                   children: <TextSpan>[
                 TextSpan(
                     text: ' Terms of Service ',
                     style: TextStyle(
                         color: Color.fromRGBO(22, 121, 29, 1),
-                        fontWeight: FontWeight.bold)),
+                        fontWeight: FontWeight.w800,
+                        fontSize: 13)),
                 TextSpan(text: 'and '),
                 TextSpan(
-                    text: 'Privacy Policy',
+                    text: 'Privacy Policy.',
                     style: TextStyle(
                         color: Color.fromRGBO(22, 121, 29, 1),
-                        fontWeight: FontWeight.bold))
+                        fontWeight: FontWeight.w800,
+                        fontSize: 13))
               ])),
         )
       ]),
